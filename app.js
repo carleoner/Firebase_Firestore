c5bd81f0468febb1b0260217af53cd95765514e8
carleoner <kubaw4000@gmail.com>
2022-09-02 15:02:19 +0200

    onSnapshot - live update

--- app.js
+++ app.js
@@ -28,15 +28,31 @@ function renderCafe(doc) {
   });
 }
 
-// getting data
+// // getting data
+// db.collection("cafes")
+//   .where("city", "==", "London")
+//   //.orderBy("name")
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.docs.forEach((doc) => {
+//       console.log(doc.data());
+//       renderCafe(doc);
+//     });
+//   });
+
+// real-time listener
 db.collection("cafes")
-  .where("city", "==", "London")
-  //.orderBy("name")
-  .get()
-  .then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      console.log(doc.data());
-      renderCafe(doc);
+  .orderBy("city")
+  .onSnapshot((snapshot) => {
+    let changes = snapshot.docChanges();
+    changes.forEach((change) => {
+      console.log(change.doc.data());
+      if (change.type == "added") {
+        renderCafe(change.doc);
+      } else if (change.type == "removed") {
+        let li = cafeList.querySelector("[data-id=" + change.doc.id + "]");
+        cafeList.removeChild(li);
+      }
     });
   });
 
