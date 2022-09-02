fded4f1f554c980f138fc046832d86039e2fbdc9
carleoner <kubaw4000@gmail.com>
2022-09-02 14:30:44 +0200

    add to firestore

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // https://github.com/iamshaunjp/firebase-firestore-playlist
 const cafeList = document.querySelector("#cafe-list");
+const form = document.querySelector("#add-cafe-form");
 
 // create element & render cafe
 function renderCafe(doc) {
@@ -26,3 +27,14 @@ db.collection("cafes")
       renderCafe(doc);
     });
   });
+
+// saving data
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("cafes").add({
+    name: form.name.value,
+    city: form.city.value,
+  });
+  form.name.value = "";
+  form.city.value = "";
+});
